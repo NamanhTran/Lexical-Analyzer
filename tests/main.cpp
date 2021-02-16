5c6bd680de7abade9b09e9c88b7ca93661e6daab
Namanh Tran <NamanhDTran@gmail.com>
2021-02-15 16:18:15 -0800

    moved unit test files into the tests folder

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "input_file.hpp"
+using namespace std;
+
+int main() {
+    string fileName = "sampleInput.txt";
+    string fileContent = inputFile(fileName);
+    cout<<fileContent;
+}
