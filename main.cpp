c1e9f06685dccf430d58e609d3bd874678d3d431
Namanh Tran <NamanhDTran@gmail.com>
2021-03-02 11:19:21 -0800

    get the file path from the command line argument rather than hardcoding the file path

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 
 #include "lexer.hpp"
 
-int main() {
-    lexer("./tests/sample_test1.txt");
+int main(int argc, char** argv) {
+    // Get the file path of the txt in the command line argument
+    std::string file_path = argv[1];
+
+    // Start the lexical analysis of the txt content
+    lexer(file_path);
 
     return 0;
 }
